5097dea1fd41a579b45ecd25485c979bfb9f40bf
Jonathan Fingold <35047199+JonathanFingold@users.noreply.github.com>
2018-06-04 14:48:34 -0700

    tidy up using statements

--- docs-samples/V4/dotnet/ContosoCafe/ContosoCafe-2-InitialInteraction/ContosoCafe/ContosoCafeBot.cs
+++ docs-samples/V4/dotnet/ContosoCafe/ContosoCafe-2-InitialInteraction/ContosoCafe/ContosoCafeBot.cs
@@ -1,9 +1,9 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.Bot;
+﻿using Microsoft.Bot;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Core.Extensions;
 using Microsoft.Bot.Schema;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ContosoCafe
 {
